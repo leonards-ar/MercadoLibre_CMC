b71af8059d02ff97f6916c2551573bfe5a5f932b
Federico Barrera Oro <federico.barreraoro@mulesoft.com>
2012-05-10 04:01:18 +0000

    Auditory AccountanPeriod.goovy: New domain class for Accountant periods. Auditory.groovy: New object that contains the auditory. Auditorytype.groovy: Type of Auditory AccountantPeriodTest, AuditoryTest, AuditoryTypeTest: New test classes

---
+++ grails-app/domain/com/ml/cmc/AccountantPeriod.groovy
@@ -0,0 +1,21 @@
+package com.ml.cmc
+
+class AccountantPeriod {
+
+	Date startDate
+	Date endDate
+	String status
+	
+    static constraints = {
+    }
+	
+	static mapping = {
+		table 'F_PERIODOS_CONTABLES'
+		version false
+		
+		id column:'CD_PERIODO', generator:'assigned'
+		startDate column:'FC_DESDE'
+		endDate column:'FC_HASTA'
+		status column:'TX_ESTADO'
+	}
+}
---
+++ grails-app/domain/com/ml/cmc/Auditory.groovy
@@ -0,0 +1,35 @@
+package com.ml.cmc
+
+class Auditory {
+
+	Date auditoryDate
+	String time
+	String user
+	AuditoryType auditoryType
+	Medio medio
+	String description
+	String rollback
+	Long rollbackLot
+	AccountantPeriod period
+	
+    static constraints = {
+    }
+	
+	static mapping = {
+		table 'F_AUDITORIA'
+		version false
+		
+		id column:'LOTE', generator:'assigned'
+		auditoryDate column:'FC_FECHA'
+		time column:'HORA'
+		user column:'TX_USUARIO'
+		auditoryType column:'TX_TIPO_AUDITORIA'
+		medio column:'CD_MEDIO'
+		description column:'TX_DESC_AUDITORIA'
+		rollback column:'FL_ROLLBACK'
+	    rollbackLot column:'LOTE_ROLLBACK'
+		period column:'CD_PERIODO'		
+	}
+	
+	
+}
