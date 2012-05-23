010be4284bb322d21784a10634f1e41a4ccfbb5d
Federico Barrera Oro <federico.barreraoro@mulesoft.com>
2012-05-23 04:36:25 +0000

    Update compensation to paint in yellow when an item is selected.

--- web-app/js/compensation.js
+++ web-app/js/compensation.js
@@ -1,29 +1,9 @@
 $(function() {
 	$('#country').chainSelect('#card', cardLink, {
-		before : function(target) // before request hide the target combobox
-									// and display the loading message
-		{
-			$(target).attr("disabled", true);
-		},
-		after : function(target) // after request show the target combobox
-									// and hide the loading message
-		{
-			$(target).attr("disabled", false);
-		},
 		nonSelectedValue : '---'
 	});
 
 	$('#card').chainSelect('#site', siteLink, {
-		before : function(target) // before request hide the target combobox
-									// and display the loading message
-		{
-			$(target).attr("disabled", true);
-		},
-		after : function(target) // after request show the target combobox
-									// and hide the loading message
-		{
-			$(target).attr("disabled", false);
-		},
 		nonSelectedValue : '---'
 	});
 	
@@ -55,30 +35,62 @@ $(function() {
 			}
 		})
 	});
+	
+	$('#lock').click(function(){
+		
+		if($(this).attr('value') == 'Lock'){
+			var strdata = $('#country').attr('id') + "=" + $('#country').val();
+			strdata += "&" + $('#card').attr('id') + "=" + $('#card').val();
+			strdata += "&" + $('#site').attr('id') + "=" + $('#site').val();
+			
+	        var $processing = getProcessingDialog();
+	        
+			$.ajax({
+				type : 'POST',
+				url : lockLink,
+				data : strdata,
+				beforeSend: function() {
+				    $processing.dialog('open');
+				},
+				complete: function(){
+				    $processing.dialog('close');
+			    },
+				success : function(data) {
+			    	$('#country').attr("disabled", true);
+			    	$('#card').attr("disabled", true);
+			    	$('#site').attr("disabled", true);
+			    	$('#lock').attr("value","Unlock");
+			    	$('#tabs').html(data);
+			    	$('#tabs').tabs({
+			    		ajaxOptions: {
+			    		error : function(XMLHttpRequest, textStatus, errorThrown) {
+			    			showError(XMLHttpRequest, textStatus, errorThrown);
+			    			},
+			    			data: $('#country').attr('id') + "=" + $('#country').val() +
+			    			      "&" + $('#card').attr('id') + "=" + $('#card').val() +
+			    			      "&" + $('#site').attr('id') + "=" + $('#site').val()
+			    		}		
+			    	});			    	
+				},
+				error : function(XMLHttpRequest, textStatus, errorThrown) {
+					showError(XMLHttpRequest, textStatus,errorThrown);
+				}
+			});		
+		} else {
+			$(location).attr('href',index);
+		}
+	});
+	
+	$('.receipt_check').live('click',function() {
+		alert('si man estoy aca');
+		$(this).parent().parent().toggleClass('yellow');
+    });
+	
+	$('.salesSite_check').live('click', function() {
+		$(this).parent().parent().toggleClass('yellow');			
+		
+	});	
+	
 });
 
-function showLoading() {
-	$loading.dialog("open");
-}
-
-function closeLoading() {
-	$loading.dialog("close");
-}
-
-function lockCombo() {
-
-	$('#country').attr("disabled", true);
-	$('#card').attr("disabled", true);
-	$('#site').attr("disabled", true);
 	
-	$('#tabs').tabs({
-		ajaxOptions: {
-		error : function(XMLHttpRequest, textStatus, errorThrown) {
-			showError(XMLHttpRequest, textStatus, errorThrown);
-			},
-			data: $('#country').attr('id') + "=" + $('#country').val() +
-			      "&" + $('#card').attr('id') + "=" + $('#card').val() +
-			      "&" + $('#site').attr('id') + "=" + $('#site').val()
-		}		
-	});
-}
