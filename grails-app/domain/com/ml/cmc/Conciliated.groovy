398e2a9dc84da052b85056037798fc1b88cb8eac
Federico Barrera Oro <federico.barreraoro@mulesoft.com>
2012-08-09 11:25:08 +0000

    Desconciliation WIP

---
+++ grails-app/domain/com/ml/cmc/Conciliated.groovy
@@ -0,0 +1,28 @@
+package com.ml.cmc
+
+class Conciliated {
+
+	SalesSite sale
+	Receipt receipt
+	Long lot
+	Medio medio
+	AccountantPeriod period
+	RegisterType registerType
+	
+    static constraints = {
+		
+    }
+	
+	static mapping = {
+		table 'F_CONCILIACIONES'
+		version false
+		
+		id column:'CD_CONCILIACION', generator:'assigned'
+		sale column: 'CD_VENTA_CUOTA'
+		receipt column: 'CD_RECIBO'
+		lot column: 'LOTE'
+		medio column: 'CD_MEDIO'
+		period column: 'CD_PERIODO'
+		registerType column:'CD_TIPO_CONC'
+	}
+}
---
+++ grails-app/domain/com/ml/cmc/Desconciliation.groovy
@@ -0,0 +1,30 @@
+package com.ml.cmc
+
+class Desconciliation {
+
+ 	SalesSite sale
+	Receipt receipt
+	Long lot
+	Conciliated conciliated
+	String username
+	Medio medio
+	AccountantPeriod period
+	
+    static constraints = {
+		
+    }
+	
+	static mapping = {
+		table 'F_CONCILIACIONES'
+		version false
+		
+		id column:'CD_CONCILIACION', generator:'assigned'
+		sale column: 'CD_VENTA_CUOTA'
+		receipt column: 'CD_RECIBO'
+		lot column: 'LOTE_CONCILIACION'
+		medio column: 'CD_MEDIO'
+		period column: 'CD_PERIODO'
+		registerType column:'CD_TIPO_CONC'
+	}
+	
+}
