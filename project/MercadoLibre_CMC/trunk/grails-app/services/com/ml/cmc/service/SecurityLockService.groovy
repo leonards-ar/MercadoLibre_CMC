29f02282efa187302e4401f7a7c43a321358066c
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2012-11-11 03:52:00 +0000

    Update Preconciliation functionality

--- project/MercadoLibre_CMC/trunk/grails-app/services/com/ml/cmc/service/SecurityLockService.groovy
+++ project/MercadoLibre_CMC/trunk/grails-app/services/com/ml/cmc/service/SecurityLockService.groovy
@@ -1,5 +1,6 @@
 package com.ml.cmc.service
 
+import com.ml.cmc.AccountantPeriod
 import com.ml.cmc.Lock
 import com.ml.cmc.Medio
 import com.ml.cmc.constants.Constant
@@ -19,16 +20,13 @@ class SecurityLockService {
     Lock lockFunctionality(String username, String functionality, String sessionId, Medio medio) throws SecLockException {
         
         def lockFound = Lock.findByMedio(medio)
-        if(lockFound == null){
-            def lock = new Lock(username:username,function:functionality,sessionId:sessionId,medio:medio)
-            
-            lock.save(flush: true)
-            
-            return lock
+        if(lockFound != null){
+
+	        def funcConstraint = constraints.get(functionality) != null ? constraints.get(functionality) : []  
+	        if(funcConstraint.contains(lockFound.function) || lockFound.function == functionality ) throw new SecLockException("Resource locked by user: ${lockFound?.username}", lockFound);
+
         }
         
-        def funcConstraint = constraints.get(functionality) != null ? constraints.get(functionality) : []  
-        if(funcConstraint.contains(lockFound.function) || lockFound.function == functionality ) throw new SecLockException("Resource locked by user: ${lockFound?.username}", lockFound);
         
         def lock = new Lock(username:username, function:functionality, sessionId: sessionId, medio: medio)
         lock.save(flush: true)
@@ -36,13 +34,40 @@ class SecurityLockService {
         return lock
         
     }
+	
+	Lock lockFunctionalityByCountry(String username, String functionality, String sessionId, String country) throws SecLockException {
+		
+		def medios = Medio.findAllByCountry(country);
+		
+		def lockFound = Lock.findAllByMedioInList(medios instanceof Medio ? [medios]:medios)
+		def locks = []
+		if(lockFound.size() > 0){
+			def funcConstraint = constraints.get(functionality) != null ? constraints.get(functionality) : []
+			lockFound.each{lock ->
+				if(funcConstraint.contains(lock.function) || lock.function == functionality ) throw new SecLockException("Resource locked by user: ${lockFound?.username}", lock);
+			}
+	
+		}
+
+		//lock all medios that has the selected country	
+			medios.each{medio ->
+				def lock = new Lock(username:username,function:functionality,sessionId:sessionId,medio:medio)
+				lock.save()
+				locks.add(lock)
+			}
+		
+		return locks[0]
+		
+	}
     
     void unLockFunctionality(String sessionId) {
         
-        Lock locker = Lock.findBySessionId(sessionId)
-        if(locker != null){
-            locker.delete(flush:true)
-        }
+		Lock.executeUpdate("delete Lock l where l.sessionId = :sessionId", [sessionId:sessionId])
+ 
     }
     
 }
+
+
+
+
--- project/MercadoLibre_CMC/trunk/web-app/js/preconciliation.js
+++ project/MercadoLibre_CMC/trunk/web-app/js/preconciliation.js
@@ -1,4 +1,12 @@
 $(function() {
+	
+    var aReceiptSelected = [];
+    var aSalesSelected = [];
+    var receiptList = [];
+    var salesList = [];
+    var conciliateList = [];
+    var filterType = 'salesFilter';
+    
 	$('#country').chainSelect('#card', cardLink, {
 		nonSelectedValue : '---'
 	});
@@ -7,6 +15,33 @@ $(function() {
 		nonSelectedValue : '---'
 	});
 
+	$('#site').change(function(){
+		var site = $(this).val();
+		if(site == '---') return;
+		var strdata = "site=" + $(this).val();
+		strdata+="&country=" + $('#country').val();
+		strdata +="&card=" + $('#card').val(); 
+		$.ajax({
+			type : 'POST',
+			url : periodLink,
+			data : strdata,
+			success : function(data) {
+				var index = 0;
+				$('#period').html("");//clear old options
+				data = eval(data);//get json array
+				$('#period').get(0).add(new Option('---', '---'), document.all ? 0 : null);
+				index = 1;
+				for (i = 0; i < data.length; i++)//iterate over all options
+				{
+					var item = data[i];
+					
+					$('#period').get(0).add(new Option(item[1],item[0]), document.all ? index++ : null);
+				}
+
+		    } 
+		});
+	});	
+	
 	$('#agrupar').live({
 	click: function() {
 
@@ -20,55 +55,23 @@ $(function() {
 	            $dialog.dialog("open");
 	            return;
 		}
-		var strData = "";
-
-
-	    $('#receipt_table tbody tr:.yellow').each(function(){
-	        if(strData.length > 0){
-	            strData += "&";
-	        }
-	        strData += "receiptId=" + $(this).find('td:eq(0)').find('input:hidden').val();
-	    });
-
-        $('#sales_table tbody tr:.yellow').each(function(){
-            if(strData.length > 0){
-                strData += "&";
-            }
-            strData += "salesSiteId=" + $(this).find('td:eq(0)').find('input:hidden').val();
-        });		
-    
-        $('#preconciliate_table input:hidden').each(
-            function() {
-                if (strData.length > 0) {
-                    strData += "&";
-                }
-                strData += $(this).attr('id') + "=" + $(this).val();
-            });
-		    
-
-		$.ajax({
-		    type : 'POST',
-			url : groupLink,
-			data : strData,
-			success : function(data) {
-				$('#conciliado').fadeOut('fast',function() {
-				    $(this).html(data).fadeIn('slow');
-				});
-
-				deleteRows('#receipt_table');
-				deleteRows('#sales_table');
-				
-				createCombos('#receipt_table');
-				createCombos('#sales_table');
-				
-				
-				var balanced = 0;
-				$('#balance').html(String(balanced));
-			},
-			error : function(XMLHttpRequest,textStatus, errorThrown) {
-				showError(XMLHttpRequest,textStatus,errorThrown);
-			}
-		})
+		
+		var receiptTable = $('#receipt_table').dataTable();
+	    var receiptRows = receiptTable.$('tr.yellow');
+	    
+	    var salesTable = $('#sales_table').dataTable();
+	    var salesRows = salesTable.$('tr.yellow');
+		
+	    group(receiptRows, salesRows, $('#preconciliate_table'))
+	    
+	    aReceiptSelected = [];
+	    aSalesSelected = [];
+	    
+	    $('#balance').html("0.00");
+	    
+	    receiptTable.fnDraw();
+	    salesTable.fnDraw();
+		
 	},
 	mouseover: function() {
 		$(this).addClass("ui-state-hover");
@@ -81,91 +84,61 @@ $(function() {
 	});
 
 	$('#receipt_table tbody tr').live('click',function() {
-
+	    var id = this.id
+	    var index = jQuery.inArray(id, aReceiptSelected);
+	    
 	    $(this).toggleClass('yellow');
-    	if ($(this).hasClass('yellow')) {
-    		var monto = parseFloat($(this).find('td:eq(8)').text());
-    		var balanced = parseFloat($('#balance').text());
-    		balanced = isNaN(balanced) ? 0 : balanced;
+	    
+	    var balanced = parseFloat($('#balance').text());
+	    balanced = isNaN(balanced) ? 0 : balanced;
+	    var monto = parseFloat($(this).find('td:eq(1)').text());
+	    
+    	if (index == -1) {
     		balanced += isNaN(monto)? 0 : monto;
-    		$('#balance').html(String(balanced));
+    		aReceiptSelected.push(id)
+    		
     	} else {
-    		var monto = parseFloat($(this).find('td:eq(8)').text());
+    	    
     		var balanced = parseFloat($('#balance').text());
-    		if (!(isNaN(balanced))) {
+    		if (balanced != 0) {
     			balanced -= isNaN(monto)? 0 : monto;
-    			$('#balance').html(String(balanced));
     		}
+    		aReceiptSelected.splice( index, 1 );
     	}
+    	
+    	$('#balance').html(String(balanced.toFixed(2)));
     });
 
 	$('#sales_table tbody tr').live('click', function() {
+        var id = this.id
+        var index = jQuery.inArray(id, aSalesSelected);
+
+        var monto = parseFloat($(this).find('td:eq(1)').text());
+        var balanced = parseFloat($('#balance').text());
+        balanced = isNaN(balanced) ? 0 : balanced;
+        
 		$(this).toggleClass('yellow');
-		if ($(this).hasClass('yellow')) {
-			var monto = parseFloat($(this).parent().parent().find('td:eq(9)').text());
-			var balanced = parseFloat($('#balance').text());
-			balanced = isNaN(balanced) ? 0 : balanced;
+
+		if (index == -1) {
 			balanced -= isNaN(monto) ? 0 : monto;
-			$('#balance').html(String(balanced));
+	         aSalesSelected.push(id)
 		} else {
-			var monto = parseFloat($(this).parent().parent().find('td:eq(9)').text());
-			var balanced = parseFloat($('#balance').text());
-			if (!(isNaN(balanced))) {
+		    aSalesSelected.splice( index, 1 );
+			if (balanced != 0) {
 				balanced += isNaN(monto) ? 0 : monto;
-				$('#balance').html(String(balanced));
 			}
 		}
+	    $('#balance').html(String(balanced.toFixed(2)));
 	});
+
 	
 	$('#filterType').live('change', function(){
 		
-		var strdata = "filterType=" + $(this).val(); 
-		strdata += "&" + $('#country').attr('id') + "=" + $('#country').val();
-		strdata += "&" + $('#card').attr('id') + "=" + $('#card').val();
-		strdata += "&" + $('#site').attr('id') + "=" + $('#site').val();
-
+		filterType = $(this).val();
 		
-		$('#preconciliate_table input:hidden').each(
-				function() {
-					if (strdata.length > 0) {
-						strdata += "&";
-					}
-					strdata += $(this).attr('id') + "=" + $(this).val();
-				});
+		 $('#receipt_table').dataTable().fnDraw();
+		 $('#sales_table').dataTable().fnDraw();
 		
-		var oDiv = $('<div></div>').html('<cener><p> Procesando...' + '</p>' + $("#spinner").html() + '</cener>')
-		
-		$.ajax({
-			type : 'POST',
-			url : listReceiptsLink,
-			data : strdata,
-			beforeSend: function() {
-			    $('#receipts').html(oDiv);
-			},
-			success : function(data) {
-		    	$('#receipts').html(data);
-		    	createTable($('#receipts').find('#receipt_table'));
-			},
-			error : function(XMLHttpRequest, textStatus, errorThrown) {
-				showError(XMLHttpRequest, textStatus,errorThrown);
-			}
-		});
-		
-		$.ajax({
-			type : 'POST',
-			url : listSalesLink,
-			data : strdata,
-			beforeSend: function() {
-			    $('#sales').html(oDiv);
-			},
-			success : function(data) {
-		    	$('#sales').html(data);
-		    	createTable($('#sales').find('#sales_table'));
-			},
-			error : function(XMLHttpRequest, textStatus, errorThrown) {
-				showError(XMLHttpRequest, textStatus,errorThrown);
-			}
-		});
 		
 	});
 
@@ -173,20 +146,17 @@ $(function() {
 
 		var strdata = "";
 
-		if($('#preconciliate_table input:hidden').length == 0) {
+		if(conciliateList.length == 0) {
 			var $dialog = getDialog("No hay elementos para preconciliar");
 			$dialog.dialog('open');
 			
 			return;
 		}
 		
-		$('#preconciliate_table input:hidden').each(
-				function() {
-					if (strdata.length > 0) {
-						strdata += "&";
-					}
-					strdata += $(this).attr('id') + "=" + $(this).val();
-				});
+		strdata = "ids=" + conciliateList.join(";")
+		strdata +="&site=" + $('#site').val();
+		strdata +="&country=" + $('#country').val();
+		strdata +="&card=" + $('#card').val(); 
 
         var $processing = getProcessingDialog();
         
@@ -277,12 +247,107 @@ $(function() {
 		
 	});	
 	
+
+	$('#applyReceiptFilter').live({
+		click: function(){
+			aReceiptSelected = [];
+			oTable = $('#receipt_table').dataTable();
+			
+			oTable.fnDraw();
+		},
+		mouseover: function() {
+			$(this).addClass("ui-state-hover");
+			$(this).css("cursor","pointer");
+		},
+		  mouseout: function() {
+			$(this).removeClass("ui-state-hover");
+		}
+		
+	});
+	
+	$('#applySalesFilter').live({
+		
+		click: function(){
+			aSalesSelected = [];
+			oTable = $('#sales_table').dataTable();
+			oTable.fnDraw();
+		},
+		mouseover: function() {
+			$(this).addClass("ui-state-hover");
+			$(this).css("cursor","pointer");
+		},
+		  mouseout: function() {
+			$(this).removeClass("ui-state-hover");
+		}
+		
+	});
+
+    $('#fromReceiptTransDate').live("focus", function(){
+        $(this).datepicker({
+            dateFormat: "dd/mm/yy",
+            changeMonth: true,
+            changeYear: true,
+            showAnim: 'fadeIn',
+        	onClose: function( selectedDate ) {
+                $( "#toReceiptTransDate" ).datepicker( "option", "minDate", selectedDate );
+            }                
+        }).datepicker('show');
+    });
+
+    $('#toReceiptTransDate').live("focus", function(){
+        $(this).datepicker({
+            dateFormat: "dd/mm/yy",
+            changeMonth: true,
+            changeYear: true,
+            showAnim: 'fadeIn',
+            onClose: function( selectedDate ) {
+            	$( "#fromReceiptTransDate" ).datepicker( "option", "maxDate", selectedDate );
+            }                	
+        }).datepicker('show');
+    });
+    
+    $('#fromSalesTransDate').live("focus", function(){
+        $(this).datepicker({
+            dateFormat: "dd/mm/yy",
+            changeMonth: true,
+            changeYear: true,
+            showAnim: 'fadeIn',
+        	onClose: function( selectedDate ) {
+                $( "#toSalesTransDate" ).datepicker( "option", "minDate", selectedDate );
+            }                
+            
+        }).datepicker('show');
+    });
+
+    $('#toSalesTransDate').live("focus", function(){
+        $(this).datepicker({
+            dateFormat: "dd/mm/yy",
+            changeMonth: true,
+            changeYear: true,
+            showAnim: 'fadeIn',
+            onClose: function( selectedDate ) {
+            	$( "#fromSalesTransDate" ).datepicker( "option", "maxDate", selectedDate );
+            }                
+        }).datepicker('show');
+    });
+	
 	$('#lock').click(function(){
 		
 		if($(this).attr('value') == 'Lock'){
+			if($('#country').val()== ''  || $('#country').val()== '---' ||
+		        $('#card').val()== '' || $('#card').val()== '---' ||
+		        $('#site').val()== '' || $('#site').val()== '---' ||
+		        $('#period').val()== ''){
+		        var $dialog = getDialog(completeFilters);
+                $dialog.dialog("open");
+                return;
+		    }
+		    
 			var strdata = $('#country').attr('id') + "=" + $('#country').val();
 			strdata += "&" + $('#card').attr('id') + "=" + $('#card').val();
 			strdata += "&" + $('#site').attr('id') + "=" + $('#site').val();
+			strdata += "&" + $('#period').attr('id') + "=" + $('#period').val();
+			strdata += "&filterType = 'salesFilter'";
 			
 	        var $processing = getProcessingDialog();
 	        
@@ -300,11 +365,75 @@ $(function() {
 			    	$('#country').attr("disabled", true);
 			    	$('#card').attr("disabled", true);
 			    	$('#site').attr("disabled", true);
+		            $('#period').attr("disabled", true);			    	
 			    	$('#lock').attr("value","Unlock");
 			    	$('#myBody').html(data);
 			    	
-			    	createTable('#receipt_table');
-			    	createTable('#sales_table');
+				    $('#receipt_table').dataTable({
+				        "sDom": 'lrtip',
+				        "sPaginationType": "full_numbers",
+				        "bProcessing": true,
+				        "bServerSide": true,        
+				        "sAjaxSource": listReceiptLink,
+				        "sServerMethod": "POST",
+				        "fnServerParams": function ( aoData ) {
+				            aoData.push( { "name": "country", "value": $('#country').val() } );
+				            aoData.push( { "name": "card", "value": $('#card').val() } );
+				            aoData.push( { "name": "site", "value": $('#site').val() } );
+				            aoData.push( { "name": "period", "value": $('#period').val() } );
+				            aoData.push( { "name": "filterType","value": filterType});
+				            aoData.push( { "name": "selectedList", "value":receiptList.join(",") } );
+				            if($('#fromReceiptTransDate').val() !='') aoData.push( { "name":"fromReceiptTransDate", "value":$('#fromReceiptTransDate').val()} );
+				            if($('#toReceiptTransDate').val() !='') aoData.push( { "name":"toReceiptTransDate", "value":$('#toReceiptTransDate').val()} );
+				        },
+				        "fnRowCallback": function( nRow, aData, iDisplayIndex ) {
+
+				            var index = jQuery.inArray(aData.DT_RowId, receiptList); 
+				            if ( index !== -1 ) {
+				                $(nRow).hide();
+				            } else if ( jQuery.inArray(aData.DT_RowId, aReceiptSelected) !== -1 ) {
+				                $(nRow).addClass('yellow');
+				            }
+				        }    
+				    });
+			    	
+				    $('#sales_table').dataTable({
+				        "sDom": 'lrtip',
+				        "sPaginationType": "full_numbers",
+				        "bProcessing": true,
+				        "bServerSide": true,        
+				        "sAjaxSource": listSalesLink,
+				        "sServerMethod": "POST",
+				        "fnServerParams": function ( aoData ) {
+				            aoData.push( { "name": "country", "value": $('#country').val() } );
+				            aoData.push( { "name": "card", "value": $('#card').val() } );
+				            aoData.push( { "name": "site", "value": $('#site').val() } );
+				            aoData.push( { "name": "period", "value": $('#period').val() } );
+				            aoData.push( { "name": "selectedList", "value":salesList.join(",") } );
+				            aoData.push( { "name": "filterType","value": filterType});
+				            if($('#fromSalesTransDate').val() !='') aoData.push( { "name":"fromSalesTransDate", "value":$('#fromSalesTransDate').val()} );
+				            if($('#toSalesTransDate').val() !='') aoData.push( { "name":"toSalesTransDate", "value":$('#toSalesTransDate').val()} );
+
+				        },
+				        "fnRowCallback": function( nRow, aData, iDisplayIndex ) {
+
+				            var index = jQuery.inArray(aData.DT_RowId, salesList); 
+				            if ( index !== -1 ) {
+				                $(nRow).hide();
+				            } else if ( jQuery.inArray(aData.DT_RowId, aSalesSelected) !== -1 ) {
+				                $(nRow).addClass('yellow');
+				            }
+				        }    
+				    });
+			    	
+			    	$('#preconciliate_table').dataTable({
+			            "bPaginate":false,
+			            "bInfo":false,
+			            "bDestroy":true,
+			            "sDom": 'rt',
+			            "bAutoWidth":false
+                     });
+			    	
 				},
 				error : function(XMLHttpRequest, textStatus, errorThrown) {
 					showError(XMLHttpRequest, textStatus,errorThrown);
@@ -315,6 +444,60 @@ $(function() {
 		}
 	});
 	
+	function group(receiptRows, salesRows, conciliateTable){
+	    var oTable = conciliateTable.dataTable({	    		
+	    		"bPaginate":false,
+	            "bInfo":false,
+	            "bDestroy":true,
+	            "sDom": 'rt',
+	            "bAutoWidth":false});
+	    if(receiptRows.length > 1) {
+	        //iterate over receipt rows
+	        for(var i=0; i < receiptRows.length; i++) {
+	            var row = receiptRows[i];
+	            var columnVals = [];
+	            var tmpIds = [];
+	            $(row).find('td').each(function(){
+	                columnVals.push($(this).html());
+	            });
+	            var saleRow = salesRows[0]; 
+	            $(saleRow).find('td').each(function(){
+	                columnVals.push($(this).html());
+	            });
+	            
+	            oTable.fnAddData(columnVals);
+	            receiptList.push(row.id);
+	            salesList.push(saleRow.id);
+	            tmpIds.push(row.id);
+	            tmpIds.push(saleRow.id);
+	            conciliateList.push(tmpIds);
+	        }
+	    } else {
+	        //iterate over sales rows
+	        for(var i=0; i < salesRows.length; i++){
+
+	            var row = salesRows[i];
+	            var columnVals = [];
+	            var tmpIds = [];
+	            var receiptRow = receiptRows[0];
+	            $(receiptRow).find('td').each(function(){
+                    columnVals.push($(this).html());
+                });
+                
+                $(row).find('td').each(function(){
+                    columnVals.push($(this).html());
+                });
+                
+                oTable.fnAddData(columnVals);
+                receiptList.push(receiptRow.id);
+                salesList.push(row.id);
+                tmpIds.push(receiptRow.id);
+                tmpIds.push(row.id);
+                conciliateList.push(tmpIds);
+	            
+	        }
+	    }
+	};
 	
 });
 
