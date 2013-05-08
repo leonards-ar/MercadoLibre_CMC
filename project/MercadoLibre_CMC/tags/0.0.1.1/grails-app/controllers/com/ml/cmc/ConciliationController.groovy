87d7ac628ce6f3afb8241a0a76296c57e093dd42
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2013-05-08 04:11:46 +0000

    Deploy new Release in DEV without LDAP authentication

---
+++ project/MercadoLibre_CMC/tags/0.0.1.1/grails-app/controllers/com/ml/cmc/ConciliationController.groovy
@@ -0,0 +1,223 @@
+package com.ml.cmc
+import grails.converters.JSON
+import grails.util.GrailsUtil
+
+import org.apache.commons.logging.LogFactory
+import org.apache.jasper.compiler.Node.ParamsAction;
+
+import com.ml.cmc.constants.Constant
+import com.ml.cmc.exception.SecLockException
+
+class ConciliationController extends SessionInfoController{
+	private static final log = LogFactory.getLog(this)
+	
+	def securityLockService
+	def lotGeneratorService
+	def sessionFactory
+
+	def colNames = ["transactionDate","amount","authorization","cardNumber","customerId","documentNumber",
+		"documentId","id","ro","tid","nsu","shareNumber","shareQty","paymentDate","payment"]
+
+	def index = {
+		securityLockService.unLockFunctionality(getSessionId())
+		def countryList = Medio.withCriteria{
+			projections{
+				distinct "country"
+			}
+			order("country")
+		}
+       
+		render(view:'index', model:[countryList: countryList])
+	}
+	
+	def periods = {
+		if(params._value == '---' || params.country == null || params.card == null) return
+		def medio = Medio.find("from Medio m where m.country= :country and m.card= :card and m.site= :site", [country:params.country, card:params.card, site: params.site])
+		def period = AccountantPeriod.find("from AccountantPeriod a where a.medio= :medio and a.status= :status ", [medio: medio, status:'ACTIVO'])
+		
+		def data = [minDate:period?.startDate, maxDate:period?.endDate] 
+		render data as JSON
+		
+	}
+	
+	def lock = {
+		
+		def medio = Medio.find("from Medio m where m.country= :country and m.card= :card and m.site= :site", [country:params.country, card:params.card, site: params.site])
+		
+		if(medio == null) {
+			response.setStatus(500)
+			render message(code:"conciliation.nomedio.found.error", default:"No se encontró ningun medio", args:[params.country, params.card, params.site])
+			return
+		}
+
+		try{
+			securityLockService.lockFunctionality(getUsername(), Constant.FUNC_CONCILIATE, getSessionId(), medio)
+		}catch (SecLockException e) {
+		   def error = message(code:"conciliation.security.error" ,default:"Error",args:[e.invalidObject?.username, medio])
+		   response.setStatus(500)
+		   render error
+		   return
+		} catch (Exception e){
+			response.setStatus(500)
+			render e.message
+			return
+		}
+		
+		render(template: "conciliationBody")
+
+	}
+
+	def listReceipts = {
+        def responseMap = [:]
+        def max = params.iDisplayLength?params.iDisplayLength:maxRecords
+        def offset = params.iDisplayStart?params.iDisplayStart:0
+		
+		def colIdx = Integer.parseInt(params.iSortCol_0)
+		def colName = colNames[colIdx]
+		def sortDir = params.sSortDir_0? params.sSortDir_0:'asc'
+
+		def medio = Medio.find("from Medio m where m.country= :country and m.card= :card and m.site= :site", [country:params.country, card:params.card, site: params.site]);
+		def state3 = State.findById(3)
+
+		def accountDate = new Date().parse(dateFormat,params.period)
+		
+        def criteria = Receipt.createCriteria()
+		def receiptInstanceList = criteria.list(max:max, offset:offset){
+			 order(colName, sortDir)
+			 if(medio != null) eq('medio', medio)
+			 eq('state',state3)
+
+			 if(params.selectedList.length() > 0) {
+				 def ids = params.selectedList.split(",")
+				 not{inList('id', ids)}
+			  }
+			 if(params.fromReceiptTransDate != null && params.toReceiptTransDate != null){
+				 def fromTransDate = new Date().parse(dateFormat, params.fromReceiptTransDate)
+				 def toTransDate = new Date().parse(dateFormat, params.toReceiptTransDate)
+				 between('transactionDate', fromTransDate, toTransDate)
+			 } else {
+			 	le('transactionDate', accountDate)
+			 }
+			 if(params.fromReceiptPaymtDate != null && params.toReceiptPaymtDate != null){
+				 def fromPaymtDate = new Date().parse(dateFormat, params.fromReceiptPaymtDate)
+				 def toPaymtDate = new Date().parse(dateFormat, params.toReceiptPaymtDate)
+				 between('paymentDate', fromPaymtDate, toPaymtDate)
+			 } else {
+			 	le('paymentDate', accountDate)
+			 }
+			 if(params.minReceiptAmount != null){
+				 ge('amount', params.minReceiptAmount?.toDouble())
+			 }
+			 if(params.maxReceiptAmount != null){
+				 le('amount', params.maxReceiptAmount?.toDouble())
+			 }
+
+		}
+        
+        responseMap.aaData = serializeReceiptData(receiptInstanceList)
+        responseMap.sEcho = params.sEcho
+        responseMap.iTotalRecords = receiptInstanceList.totalCount
+        responseMap.iTotalDisplayRecords = receiptInstanceList.totalCount
+		
+		render responseMap as JSON
+		
+	}
+	
+	def listSalesSite = {
+        def responseMap = [:]
+        
+        def max = params.iDisplayLength?params.iDisplayLength:maxRecords
+        def offset = params.iDisplayStart?params.iDisplayStart:0
+		def colIdx = Integer.parseInt(params.iSortCol_0)
+		def colName = colNames[colIdx]
+		def sortDir = params.sSortDir_0? params.sSortDir_0:'asc'
+
+		def medio = Medio.findAll("from Medio m where m.country= :country and m.site= :site", [country:params.country, site: params.site])
+		def state = State.findById(3)
+		
+		def accountDate = new Date().parse(dateFormat,params.period)
+		
+        def criteria = SalesSite.createCriteria()
+		def salesSiteInstanceList = criteria.list(max:max, offset:offset) {
+			order(colName, sortDir)
+			
+			 if(medio != null) inList('medio', medio)
+			 eq('state',state)
+			 eq('origin','I')
+			 
+			 if(params.salesSelectedBox == "checked") {
+				 if(params.selectedRows.length() > 0) {
+					 def ids = params.selectedRows.split(",")
+					inList('id', ids)
+				  }
+			 } else {
+				 if(params.selectedList.length() > 0) {
+					 def ids = params.selectedList.split(",")
+					  not{inList('id', ids)}
+				}
+             }
+			  
+			 if(params.fromSalesTransDate != null && params.toSalesTransDate != null){
+			  def fromTransDate = new Date().parse(dateFormat, params.fromSalesTransDate)
+			  def toTransDate = new Date().parse(dateFormat, params.toSalesTransDate)
+			  between('transactionDate', fromTransDate, toTransDate)
+			 } else {
+			 	le('transactionDate', accountDate)
+			 }
+			 if(params.minSalesAmount != null){
+				 ge('amount', params.minSalesAmount?.toDouble())
+			 }
+			 if(params.maxSalesAmount != null){
+				 le('amount', params.maxSalesAmount?.toDouble())
+			 }
+		}
+        
+        responseMap.aaData = serializeReceiptData(salesSiteInstanceList)
+        responseMap.sEcho = params.sEcho
+        responseMap.iTotalRecords = salesSiteInstanceList.totalCount
+        responseMap.iTotalDisplayRecords = salesSiteInstanceList.totalCount
+        
+        render responseMap as JSON
+		
+		
+	}
+	
+	def save = { 
+		def lot = lotGeneratorService.getLotId()
+		
+		List salesSiteReceiptList = params.ids.split(";")
+		def medio = Medio.find("from Medio m where m.country= :country and m.card= :card and m.site= :site", [country:params.country, card:params.card, site: params.site]);
+		
+		Conciliation.withTransaction{
+			
+			salesSiteReceiptList.each{ item ->
+				def conciliateIds = item.split(",")
+				def receipt = Receipt.findById(conciliateIds[0])
+				def salesSite = SalesSite.findById(conciliateIds[1])
+				def conciliation = new Conciliation(sale:salesSite, receipt:receipt,
+					lot:lot, medio:medio, period:receipt?.period, registerType:receipt?.registerType)
+				
+				conciliation.save()
+			}
+		}
+		
+		sessionFactory.getCurrentSession().clear();
+		
+		
+		/* call datastage */
+		def username = getUsername()
+		def strLot = formatNumber(number:lot, format:"000")
+		def accountDate = formatDate(date:new Date().parse('yyyy-MM-dd',params.period),format:'yyyy-MM_dd')
+		def command = "/datastage/ConcManual.sh" + (GrailsUtil.getEnvironment().equals('mercadolibre') ? "_PROD":"") 
+		Thread.start{
+				executeCommand("${command} ${username} ${strLot} ${accountDate}")
+		}
+		
+		
+		render message(code:"conciliation.calledProcess", default:"Se ha invocado el proceso", args:[username])
+		 
+	}
+    
+
+	
+}
---
+++ project/MercadoLibre_CMC/tags/0.0.1.1/grails-app/controllers/com/ml/cmc/SessionInfoController.groovy
@@ -0,0 +1,140 @@
+package com.ml.cmc
+import grails.converters.JSON
+
+import java.security.Principal
+
+import org.apache.commons.logging.LogFactory
+
+class SessionInfoController {
+	//private static final myLog = LogFactory.getLog(this)
+	
+    def sessionRegistry
+    def springSecurityService
+	def securityLockService
+	
+	def dateFormat = 'yyyy-MM-dd'
+	def maxRecords = 50
+    protected String getSessionId() {
+        
+        return org.springframework.web.context.request.RequestContextHolder.getRequestAttributes()?.getSessionId()
+        
+    }
+    
+    protected String getUsername() {
+        
+        def principal = springSecurityService.principal
+        return principal.username
+        
+    }
+    
+    protected Principal getPrincipal() {
+        
+        return springSecurityService.principal
+        
+    }
+    
+    protected Lock getLocker() {
+        
+        def locker = new Lock()
+        locker.sessionId = getSessionId()
+        locker.username = getUsername()
+        locker.function = 'pre_conciliate'
+    
+        return locker;
+        
+    }
+
+	def cards = {
+		
+		def cardsToSelect = Medio.withCriteria{
+			projections{
+				distinct "card"
+			}
+			eq("country", params._value)
+			order("card")
+		}
+		
+		render cardsToSelect as JSON
+	}
+	
+	def sites = {
+		
+		 def sitesToSelect = Medio.withCriteria{
+			projections{
+				distinct "site"
+			}
+			eq("card", params._value)
+			order("site")
+		}
+		render sitesToSelect as JSON
+	}
+	
+	def periods = {
+		if(params._value == '---' || params.country == null || params.card == null) return
+		def medio = Medio.find("from Medio m where m.country= :country and m.card= :card and m.site= :site", [country:params.country, card:params.card, site: params.site])
+		def periodList = AccountantPeriod.findAll("from AccountantPeriod a where a.medio= :medio and a.status= :status ", [medio: medio, status:'ACTIVO'])
+		def periodKey = []
+		periodList.each { item ->
+			periodKey.add([item.id, item.toString()])
+			
+		}
+		render periodKey as JSON
+		
+	}
+		
+	def exit = {
+		securityLockService.unLockFunctionality(getSessionId())
+		redirect(controller:'home', action:'index')
+	}
+	
+	protected serializeReceiptData(instanceList) {
+		
+		def data = []
+		
+		instanceList.each(){
+			data << ["DT_RowId":it.id.toString(),
+					 "0":formatDate(date:it?.transactionDate, format:"dd-MM-yyyy"),
+					 "1":formatNumber(number:it?.amount,format:"###,###.00"),
+					 "2":it?.authorization.toString(),
+					 "3":it?.cardNumber.toString(),
+					 "4":it?.customerId.toString(),
+					 "5":it?.documentNumber.toString(),
+					 "6":it?.documentId.toString(),
+					 "7":it?.id.toString(),
+					 "8":it?.ro.toString(),
+					 "9":it?.tid.toString(),
+					 "10":it?.nsu.toString(),
+					 "11":it?.shareNumber.toString(),
+					 "12":it?.shareQty.toString(),
+					 "13":formatDate(date:it?.paymentDate, format:"dd-MM-yyyy"),
+					 "14":it?.payment
+					 ]
+		}
+		
+		return data
+	}
+	
+	protected Integer executeCommand(String command) {
+		
+		StringWriter stringWriterOutput = new StringWriter()
+		StringWriter stringWriterError = new StringWriter()
+
+		def batchProcess = "${command}"
+		log.info("Callling batch process ${batchProcess}")
+
+		Process job = command.execute()
+		
+		stringWriterOutput << job.in
+		stringWriterError << job.err
+		
+		job.waitFor()
+		
+		log.info("Process \"${batchProcess}\" returned: ${job.exitValue()}")
+		log.info("Process \"${batchProcess}\" stdout: ${stringWriterOutput.toString()}")
+		log.info("Process \"${batchProcess}\" stderr: ${stringWriterError.toString()}")
+		
+		return job.exitValue()
+		
+	}
+    
+}
