e5092e1b360c2b7bf5b822953a9adccce47dd82b
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2013-05-27 04:22:35 +0000

    Create new Release with a fixed in login.

---
+++ project/MercadoLibre_CMC/tags/0.1.1/grails-app/domain/com/ml/cmc/Conciliation.groovy
@@ -0,0 +1,28 @@
+package com.ml.cmc
+
+class Conciliation implements Serializable {
+
+	SalesSite sale
+	Receipt receipt
+	Long lot
+	Medio medio
+	AccountantPeriod period
+	RegisterType registerType
+	
+    static constraints = {
+		
+    }
+	
+	static mapping = {
+		table 'F_TMP_CONCILIACIONES'
+		version false
+		
+		id composite: ['lot','sale','receipt']
+		sale column: 'CD_VENTA_CUOTA'
+		receipt column: 'CD_RECIBO'
+		lot column: 'SERIAL_JAVA'
+		medio column: 'CD_MEDIO'
+		period column: 'CD_PERIODO'
+		registerType column:'CD_TIPO_CONC'
+	}
+}
---
+++ project/MercadoLibre_CMC/tags/0.1.1/test/unit/com/ml/cmc/DespreconciliationControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.ml.cmc
+
+import grails.test.*
+
+class DespreconciliationControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
