242b2872b2c090406dc75822e0a4f1d24c3b03a7
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2013-05-27 05:13:30 +0000

    Remove unnecessary class. Update application version using grails set-version

---
+++ project/MercadoLibre_CMC/tags/0.1.1/grails-app/services/com/ml/cmc/service/LotGeneratorService.groovy
@@ -0,0 +1,27 @@
+package com.ml.cmc.service
+
+class LotGeneratorService {
+
+    static transactional = true
+    
+    def sessionFactory
+
+    def getLotId() {
+        
+        def sql = "SELECT JAVA_LOT.nextval FROM dual"
+        def session = sessionFactory.getCurrentSession();
+        def query = session.createSQLQuery(sql);
+        def result = query.list()
+        return result[0]
+    }
+    
+    def getGroupId() {
+        
+        def sql = "SELECT COMP_GROUP.nextval FROM dual"
+        def session = sessionFactory.getCurrentSession();
+        def query = session.createSQLQuery(sql);
+        def result = query.list()
+        return result[0]
+        
+    }
+}
