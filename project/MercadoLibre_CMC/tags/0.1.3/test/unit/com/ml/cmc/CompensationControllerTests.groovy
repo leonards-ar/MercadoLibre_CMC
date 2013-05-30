87b0d7ce6310d6aa4f84ff3167a89f96a463b133
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2013-05-30 01:39:25 +0000

    New Release for production

---
+++ project/MercadoLibre_CMC/tags/0.1.3/test/unit/com/ml/cmc/CompensationControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.ml.cmc
+
+import grails.test.*
+
+class CompensationControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
---
+++ project/MercadoLibre_CMC/tags/0.1.3/test/unit/com/ml/cmc/service/LotGeneratorServiceTests.groovy
@@ -0,0 +1,17 @@
+package com.ml.cmc.service
+
+import grails.test.*
+
+class LotGeneratorServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
